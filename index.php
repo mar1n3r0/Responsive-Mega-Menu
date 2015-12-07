c01e5f9ad03de1f2146d73036d9b8546552f2219
mar1n3r0 <m.kirkov@gmail.com>
2015-12-07 18:34:07 +0200

    Second commit

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/main.css"> 
+    <link rel="stylesheet" href="css/media-queries.css">
+	<title>Mega Menu</title>
+</head>
+<body>
+<ul id="menu">
+    <li><a href="#">Home</a>
+    </li>
+    <li><a href="#">About us</a>
+    </li>
+    <li class="drop"><a href="#">Products</a>
+    <img class="caret" src="assets/ico-caret.png">
+    	<div class="dropdown_4columns">
+        	<div class="banner">
+        		<a href="#" class="promo"><img class="bonus" src="assets/banner.png"></a>
+        	</div>
+        	<div class="col_1">
+        		<header>
+        			<a href="#">
+        				<img src="assets/ico-grid.png" class="bonus">
+      					<h3>SPORTSBOOK</h3>
+      				</a>
+      				<hr>
+      				<nav>
+        				<ul class="inner-menu">
+        					<li><a href="#">Sportsbook</a></li>
+        					<li><a href="#">White Label</a></li>
+        					<li><a href="#">Mobile</a></li>
+        				</ul>
+      				</nav>
+    			</header>
+    			<a href="#"><img align="center" src="assets/img-mobile.jpg" class="thumb"></a>
+        	</div>
+        	<div class="col_1">
+        		<header>
+        			<a href="#">
+        				<img src="assets/ico-star.png" class="bonus">
+      					<h3>CASINO GAMES</h3>
+      				</a>
+      				<hr>
+      				<nav>
+        				<ul class="inner-menu">
+        					<li><a href="#">Slot Games</a></li>
+        					<li><a href="#">Casino Games</a></li>
+        				</ul>
+      				</nav>
+      				<a href="#" id="special"><img src="assets/ico-start-circle.png">New Games</a>
+    			</header>	
+    			<a href="#"><img align="center" src="assets/img-slots.jpg" class="thumb"></a>
+        	</div>
+        	<div class="col_1">
+        		<header>
+        			<a href="#">
+        				<img src="assets/ico-medal.png" class="bonus">
+      					<h3>VIRTUAL GAMES</h3>
+      				</a>
+      				<hr>
+      				<nav>
+        				<ul class="inner-menu">
+        					<li><a href="#">Horse Racing</a></li>
+        					<li><a href="#">Dog Racing</a></li>
+        				</ul>
+      				</nav>
+    			</header>
+    			<a href="#"><img align="center" src="assets/img-horse.jpg" class="thumb"></a>
+        	</div>
+        	<div class="col_1">
+        		<header>
+        			<a href="#">
+        				<img src="assets/ico-asterisk.png" class="bonus">
+      					<h3>DRAW GAMES</h3>
+      				</a>
+      				<hr>
+      				<nav>
+        				<ul class="inner-menu">
+        					<li><a href="#">Keno</a></li>
+        					<li><a href="#">Lotteries</a></li>
+        				</ul>
+      				</nav>
+      				<a href="#" id="special"><img src="assets/ico-start-circle.png">Live Draws</a>
+    			</header>
+    			<a href="#"><img align="center" src="assets/img-lottery.jpg" class="thumb"></a>
+        	</div>
+        </div>
+    </li>
+    <li><a href="#">System</a>
+    </li>
+    <li><a href="#">News</a>
+    </li>
+    <li><a href="#">Contacts</a>
+    </li>
+    <li><a href="#">Careers</a>
+    </li>
+</ul>
+</body>
+</html>
